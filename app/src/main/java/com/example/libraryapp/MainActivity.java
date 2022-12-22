a46311790ba3b6ee98980ba6d6941524d6ef8d2d
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2022-12-22 16:41:09 +0000

    Setup

--- app/src/main/java/com/example/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/libraryapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.libraryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginButton(){
+        Intent signupIntent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(signupIntent);
+    }
+
+    public void signupButton(){
+        Intent signupIntent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(signupIntent);
     }
 }
