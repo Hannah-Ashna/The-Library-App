d7bfb7a91c0c45bdb4b488c750d30eb83b15dcad
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2022-12-27 23:02:49 +0000

    Update SettingsFragment.java

--- app/src/main/java/com/example/libraryapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/libraryapp/ui/settings/SettingsFragment.java
@@ -1,20 +1,34 @@
 package com.example.libraryapp.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.libraryapp.MainActivity;
+import com.example.libraryapp.R;
+import com.example.libraryapp.SignUpActivity;
 import com.example.libraryapp.databinding.FragmentSettingsBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -26,6 +40,44 @@ public class SettingsFragment extends Fragment {
 
         final TextView textView = binding.textSettings;
         settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button signOutButton = (Button)root.findViewById(R.id.settingsSignOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(getActivity()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+
+        Button deleteButton = (Button)root.findViewById(R.id.settingsDeleteAccButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Delete User Data from Database
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                db.collection("Users").document(user.getUid()).delete().addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Settings Fragment", "Error delete user's document", e);
+                    }});
+
+                AuthUI.getInstance().delete(getActivity()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+
+                    }
+                });
+            }
+        });
+
         return root;
     }
 
@@ -35,4 +87,8 @@ public class SettingsFragment extends Fragment {
         binding = null;
     }
 
+    @Override
+    public void onClick(View view) {
+        // Leave Blank
+    }
 }
