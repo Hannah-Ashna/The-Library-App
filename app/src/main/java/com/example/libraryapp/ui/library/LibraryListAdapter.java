786aea65d577aa74fe6140972f87df01a9f9e80c
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2022-12-29 15:32:42 +0000

    Update LibraryListAdapter.java

--- app/src/main/java/com/example/libraryapp/ui/library/LibraryListAdapter.java
+++ app/src/main/java/com/example/libraryapp/ui/library/LibraryListAdapter.java
@@ -1,16 +1,13 @@
 package com.example.libraryapp.ui.library;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.libraryapp.R;
-import java.util.ArrayList;
 import java.util.List;
 
 public class LibraryListAdapter extends BaseAdapter {
