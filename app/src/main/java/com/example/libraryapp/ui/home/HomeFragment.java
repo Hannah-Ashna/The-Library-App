fdf555cc6d81583c5f484358501febd28258709a
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2023-01-13 10:50:20 +0000

    Foundations for NFC capabilities

--- app/src/main/java/com/example/libraryapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/libraryapp/ui/home/HomeFragment.java
@@ -1,28 +1,21 @@
 package com.example.libraryapp.ui.home;
 
 import android.content.Intent;
-import android.media.Image;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.libraryapp.MainActivity;
 import com.example.libraryapp.R;
 import com.example.libraryapp.databinding.FragmentHomeBinding;
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,17 +24,18 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
     private FragmentHomeBinding binding;
 
+    // NFC Variables
+    public static final String TAG = "DevSocBook";
+    private NfcAdapter NFCAdapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        
-        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        NFCAdapter = NfcAdapter.getDefaultAdapter(getActivity());
+
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        //final TextView textView = binding.textHome;
-        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         // Activate NFC Scanning Feature
         ImageView activateNFC = (ImageView)root.findViewById(R.id.bookNFCButton);
@@ -50,14 +44,37 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             public void onClick(View view) {
                 Animation animation = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.blink);
                 activateNFC.startAnimation(animation);
-                Snackbar scanSnackBar = Snackbar.make(view, "Scanning for Book NFC", Snackbar.LENGTH_LONG);
+                Snackbar scanSnackBar = Snackbar.make(view, "Scanning for Book NFC", Snackbar.LENGTH_INDEFINITE);
                 scanSnackBar.show();
 
                 // Do NFC Scanning Logic then stop animation upon success or failure
+                if (NFCAdapter == null){
+                    // Do not proceed, NFC is a necessary permission for this feature
+                    scanSnackBar.dismiss();
+                    Toast.makeText(getActivity(), "This device does not support NFC", Toast.LENGTH_LONG).show();
+                    animation.reset();
+                    return;
+                }
+
+                if (NFCAdapter.isEnabled()){
+                    animation.reset();
+                    scanSnackBar.dismiss();
+                    Toast.makeText(getActivity(), "NFC is disabled", Toast.LENGTH_LONG).show();
+                } else {
+                    animation.cancel();
+                    scanSnackBar.dismiss();
+                    Toast.makeText(getActivity(), "NFC is enabled!", Toast.LENGTH_LONG).show();
+                }
+
+                handleIntent(getActivity().getIntent());
             }
         });
 
         return root;
+    }
+
+    private void handleIntent (Intent intent){
+        // Pending Stuff
     }
 
     @Override
