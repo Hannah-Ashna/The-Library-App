5ef06e9211f909f5d89a1470116a13afbaa16448
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2023-02-02 21:13:17 +0000

    Attempting to implement NFCs again

--- app/src/main/java/com/example/libraryapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/libraryapp/ui/home/HomeFragment.java
@@ -1,19 +1,28 @@
 package com.example.libraryapp.ui.home;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
 import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,14 +34,32 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 public class HomeFragment extends Fragment implements View.OnClickListener{
 
     private FragmentHomeBinding binding;
 
+    // NFC Messages
+    public static final String Error_Detected = "No NFC Tag Detected";
+    public static final String Write_Success = "Text Written Successfully";
+    public static final String Write_Error = "Error during Write - Try Again";
+
     // NFC Variables
-    public static final String TAG = "DevSocBook";
     private NfcAdapter NFCAdapter;
     PendingIntent pendingIntent;
+    IntentFilter writingTagFilters[];
+    boolean writeMode;
+    Tag NFCTag;
+    Context context;
+
+    // REMOVE SOON?
+    TextView edit_message;
+    TextView nfc_contents;
+    Button nfc_button;
+
+    // Other Variables
     Snackbar scanSnackBar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -44,42 +71,151 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // --- TEMP STUFF
+        edit_message = (TextView)root.findViewById(R.id.edit_message);
+        nfc_contents = (TextView)root.findViewById(R.id.nfc_Contents);
+        nfc_button = (Button)root.findViewById(R.id.button2);
+        context = this.getContext();
+        // --- TEMP STUFF
+
         // Activate NFC Scanning Feature
         ImageView activateNFC = (ImageView)root.findViewById(R.id.bookNFCButton);
         activateNFC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Do NFC Scanning Logic then stop animation upon success or failure
-                if (NFCAdapter == null){
-                    // Do not proceed, NFC is a necessary permission for this feature
-                    scanSnackBar = Snackbar.make(view, "This device does not support NFC scanning", Snackbar.LENGTH_LONG);
-                    scanSnackBar.show();
-                    return;
+                try {
+                    if (NFCTag == null){
+                        Toast.makeText(context, Error_Detected, Toast.LENGTH_LONG).show();
+                    } else {
+                        write("PlainText |" + edit_message.getText().toString(), NFCTag);
+                        Toast.makeText(context, Write_Success, Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException e){
+                    Toast.makeText(context, Write_Error, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                } catch (FormatException e){
+                    Toast.makeText(context, Write_Error, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
-
-                if (!NFCAdapter.isEnabled()){
-                    scanSnackBar = Snackbar.make(view, "NFC currently disabled", Snackbar.LENGTH_LONG);
-                    scanSnackBar.show();
-                } else {
-                    Animation animation = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.blink);
-                    activateNFC.startAnimation(animation);
-                    scanSnackBar = Snackbar.make(view, "Scanning for Book NFC", Snackbar.LENGTH_INDEFINITE);
-                    scanSnackBar.show();
-                }
-
-                pendingIntent.getActivity(getActivity(), 0, new Intent(getActivity(), getActivity().getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
-                //handleIntent(getActivity().getIntent());
-
             }
         });
+
+
+        if (NFCAdapter == null){
+            // Do not proceed, NFC is a necessary permission for this feature
+            //scanSnackBar = Snackbar.make(view, "This device does not support NFC scanning", Snackbar.LENGTH_LONG);
+            //scanSnackBar.show();
+            Log.e("Hmmm","NULL");
+        }
+
+        if (!NFCAdapter.isEnabled()){
+            //scanSnackBar = Snackbar.make(view, "NFC currently disabled", Snackbar.LENGTH_LONG);
+            //scanSnackBar.show();
+        } else {
+            Animation animation = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.blink);
+            activateNFC.startAnimation(animation);
+            //scanSnackBar = Snackbar.make(view, "Scanning for Book NFC", Snackbar.LENGTH_INDEFINITE);
+            //scanSnackBar.show();
+
+            readFromIntent(getActivity().getIntent());
+            pendingIntent.getActivity(getActivity(), 0, new Intent(getActivity(), getActivity().getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+            IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+            tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+            writingTagFilters = new IntentFilter[] {tagDetected};
+        }
+
 
         return root;
     }
 
-    private void handleIntent (Intent intent){
-        // Pending Stuff
+    private void readFromIntent (Intent intent){
+        String action = intent.getAction();
+        if (NFCAdapter.ACTION_TAG_DISCOVERED.equals(action)
+            || NFCAdapter.ACTION_TECH_DISCOVERED.equals(action)
+            || NFCAdapter.ACTION_NDEF_DISCOVERED.equals(action)){
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NFCAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++){
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
     }
 
+    private void buildTagViews (NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0)? "UTF-8" : "UTF-16";
+        int languageCodeLength = payload[0] & 0063;
+
+        try {
+            // Obtain the text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (Exception e){
+            Log.e("Unsupported Encoding:", e.toString());
+        }
+
+        nfc_contents.setText("NFC Content: " + text);
+    }
+
+    private void write (String text, Tag tag) throws IOException, FormatException {
+        NdefRecord[] records = { createRecord(text)};
+        NdefMessage message = new NdefMessage(records);
+
+        Ndef ndef = Ndef.get(tag);
+        ndef.connect();
+        ndef.writeNdefMessage(message);
+        ndef.close();
+    }
+
+    private NdefRecord createRecord (String text) throws UnsupportedEncodingException {
+        String lang = "en";
+        byte[] textBytes = text.getBytes();
+        byte[] langBytes = lang.getBytes("US-ASCII");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+        byte[] payload = new byte[1 + langLength + textLength];
+
+        payload[0] = (byte) langLength;
+
+        System.arraycopy(langBytes, 0, payload, 1, langLength);
+        System.arraycopy(textBytes, 0, payload,1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
+
+        return recordNFC;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        WriteModeOff();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        WriteModeOn();
+    }
+
+    private void WriteModeOn(){
+        writeMode = true;
+        NFCAdapter.enableForegroundDispatch(getActivity(), pendingIntent, writingTagFilters, null);
+    }
+
+    private void WriteModeOff(){
+        writeMode = false;
+        NFCAdapter.disableForegroundDispatch(getActivity());
+    }
+
+
+    // Some Extra Bits - Can ignore for now
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -91,164 +227,4 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         // Leave Blank
     }
 
-    // Continue listening for NFC tag
-    @Override
-    public void onResume(){
-        super.onResume();
-        assert NFCAdapter != null;
-        NFCAdapter.enableForegroundDispatch(getActivity(), pendingIntent, null,null);
-    }
-
-    // Stop listening for NFC tag
-    @Override
-    public void onPause(){
-        super.onPause();
-        if (NFCAdapter != null){
-            NFCAdapter.disableForegroundDispatch(getActivity());
-        }
-    }
-
-    protected void onNewIntent(Intent intent){
-        super.onNewIntent(intent);
-        setIntent(intent);
-        resolveIntent(intent);
-    }
-
-    private void resolveIntent(Intent intent){
-        String action = intent.getAction();
-        if (NFCAdapter.ACTION_TAG_DISCOVERED.equals(action) ||
-            NFCAdapter.ACTION_TECH_DISCOVERED.equals(action) ||
-            NFCAdapter.ACTION_NDEF_DISCOVERED.equals(action)){
-
-            Tag tag = (Tag) intent.getParcelableExtra(NFCAdapter.EXTRA_TAG);
-            assert tag != null;
-            byte[] payload = detectTagData(tag).getBytes();
-        }
-    }
-
-
-    // Extra stuff
-    private String detectTagData(Tag tag) {
-        StringBuilder sb = new StringBuilder();
-        byte[] id = tag.getId();
-        sb.append("ID (hex): ").append(toHex(id)).append('\n');
-        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n');
-        sb.append("ID (dec): ").append(toDec(id)).append('\n');
-        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n');
-
-        String prefix = "android.nfc.tech.";
-        sb.append("Technologies: ");
-        for (String tech : tag.getTechList()) {
-            sb.append(tech.substring(prefix.length()));
-            sb.append(", ");
-        }
-
-        sb.delete(sb.length() - 2, sb.length());
-
-        for (String tech : tag.getTechList()) {
-            if (tech.equals(MifareClassic.class.getName())) {
-                sb.append('\n');
-                String type = "Unknown";
-
-                try {
-                    MifareClassic mifareTag = MifareClassic.get(tag);
-
-                    switch (mifareTag.getType()) {
-                        case MifareClassic.TYPE_CLASSIC:
-                            type = "Classic";
-                            break;
-                        case MifareClassic.TYPE_PLUS:
-                            type = "Plus";
-                            break;
-                        case MifareClassic.TYPE_PRO:
-                            type = "Pro";
-                            break;
-                    }
-                    sb.append("Mifare Classic type: ");
-                    sb.append(type);
-                    sb.append('\n');
-
-                    sb.append("Mifare size: ");
-                    sb.append(mifareTag.getSize() + " bytes");
-                    sb.append('\n');
-
-                    sb.append("Mifare sectors: ");
-                    sb.append(mifareTag.getSectorCount());
-                    sb.append('\n');
-
-                    sb.append("Mifare blocks: ");
-                    sb.append(mifareTag.getBlockCount());
-                } catch (Exception e) {
-                    sb.append("Mifare classic error: " + e.getMessage());
-                }
-            }
-
-            if (tech.equals(MifareUltralight.class.getName())) {
-                sb.append('\n');
-                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
-                String type = "Unknown";
-                switch (mifareUlTag.getType()) {
-                    case MifareUltralight.TYPE_ULTRALIGHT:
-                        type = "Ultralight";
-                        break;
-                    case MifareUltralight.TYPE_ULTRALIGHT_C:
-                        type = "Ultralight C";
-                        break;
-                }
-                sb.append("Mifare Ultralight type: ");
-                sb.append(type);
-            }
-        }
-        Log.v("test",sb.toString());
-        return sb.toString();
-    }
-    private String toHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = bytes.length - 1; i >= 0; --i) {
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-            if (i > 0) {
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-    }
-
-    private String toReversedHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bytes.length; ++i) {
-            if (i > 0) {
-                sb.append(" ");
-            }
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-        }
-        return sb.toString();
-    }
-
-    private long toDec(byte[] bytes) {
-        long result = 0;
-        long factor = 1;
-        for (int i = 0; i < bytes.length; ++i) {
-            long value = bytes[i] & 0xffl;
-            result += value * factor;
-            factor *= 256l;
-        }
-        return result;
-    }
-
-    private long toReversedDec(byte[] bytes) {
-        long result = 0;
-        long factor = 1;
-        for (int i = bytes.length - 1; i >= 0; --i) {
-            long value = bytes[i] & 0xffl;
-            result += value * factor;
-            factor *= 256l;
-        }
-        return result;
-    }
 }
